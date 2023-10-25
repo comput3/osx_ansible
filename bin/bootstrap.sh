b6a9830afe0c0cd14dcbf2948e397a5c363506a9
comput3 <74689393+comput3@users.noreply.github.com>
2023-10-25 15:03:08 -0700

    Create bootstrap.sh

---
+++ bin/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xcode-select --install
+sudo xcodebuild -license
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+sudo pip3 install --ignore-installed ansible
+ansible-galaxy install -r requirements.yml
+
+ansible-playbook -i "localhost," -c local osx_ansible.yml --ask-become-pass
