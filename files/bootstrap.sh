8ec10c486ed10cf61bf179f350b3b94116952dd2
comput3 <74689393+comput3@users.noreply.github.com>
2023-10-26 09:14:02 -0700

    Update bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 xcode-select --install
 sudo xcodebuild -license
+/usr/sbin/softwareupdate --install-rosetta
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 sudo pip3 install --ignore-installed ansible
