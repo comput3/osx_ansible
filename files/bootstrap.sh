def9ee15fc2d2b72641e601ec3bca775a9617e07
comput3 <74689393+comput3@users.noreply.github.com>
2023-10-27 21:01:01 -0700

    Update bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,10 +1,40 @@
 #!/bin/sh
-xcode-select --install
-sudo xcodebuild -license
-/usr/sbin/softwareupdate --install-rosetta
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-sudo pip3 install --ignore-installed ansible
+
+# Check if Xcode Command Line Tools are installed
+if ! xcode-select -p &>/dev/null; then
+    xcode-select --install
+else
+    echo "Xcode Command Line Tools already installed."
+fi
+
+# Accept Xcode license
+sudo xcodebuild -license accept
+
+# Install Rosetta if not installed
+if ! arch -x86_64 /usr/bin/true &>/dev/null; then
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+else
+    echo "Rosetta already installed."
+fi
+
+# Install pip for Python3
+if ! command -v pip3 &>/dev/null; then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py
+    rm get-pip.py
+else
+    echo "pip3 already installed."
+fi
+
+# Install Ansible
+if ! command -v ansible &>/dev/null; then
+    sudo pip3 install --ignore-installed ansible
+else
+    echo "Ansible already installed."
+fi
+
+# Install Ansible roles from requirements
 ansible-galaxy install -r requirements.yml
 
+# Execute Ansible playbook
 ansible-playbook -i "localhost," -c local playbooks/configure.yml --ask-become-pass
